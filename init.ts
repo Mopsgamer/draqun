9f2028f7e806b51b5145975d126ebf70974718cf
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-11-13 18:13:18 +0100

    change init commets

--- init.ts
+++ init.ts
@@ -6,13 +6,13 @@ function initEnv(): void {
         comment?: string
     }
     const defaultEnv = new Map<string, EnvKeyEntry>()
-    defaultEnv.set("ENVIRONMENT", { value: "1", comment: "can be test, dev or prod" })
-    defaultEnv.set("JWT_KEY", { comment: "use any online jwt generator" })
-    defaultEnv.set("DB_PASSWORD", {})
-    defaultEnv.set("DB_NAME", { value: "restapp" })
-    defaultEnv.set("DB_USER", { value: "root" })
-    defaultEnv.set("DB_HOST", { value: "localhost" })
-    defaultEnv.set("DB_PORT", { value: "3306" })
+    defaultEnv.set("ENVIRONMENT", { value: "1", comment: "can be 0 (test), 1 (dev) or 2 (prod)\ndefault: 1" })
+    defaultEnv.set("JWT_KEY", { comment: "use any online jwt generator to fill this value" })
+    defaultEnv.set("DB_PASSWORD", { comment: "connection password" })
+    defaultEnv.set("DB_NAME", { value: "restapp", comment: "connection name\ndefault: restapp" })
+    defaultEnv.set("DB_USER", { value: "root", comment: "connection user\nroot user is not recommended\ndefault: root" })
+    defaultEnv.set("DB_HOST", { value: "localhost", comment: "connection host\ndefault: localhost" })
+    defaultEnv.set("DB_PORT", { value: "3306", comment: "connection port\ndefault: 3306" })
 
     const path = ".env";
     const decoder = new TextDecoder();
@@ -24,7 +24,7 @@ function initEnv(): void {
         encoder.encode(
             Array.from(defaultEnv.entries()).map(
                 ([key, { value, comment }]) =>
-                    `${comment ? "# " + comment + "\n" : ""}${key}=${env[key] || value || ""}\n`
+                    `${comment ? "# " + comment.replace('\n', '\n# ') + "\n" : ""}${key}=${env[key] || value || ""}\n\n`
             ).join("")
         ),
     );
