ec3bc2ca7c58d99a4b927a644a43ea3bf4d15e09
Mopsgamer <kisillev020304@gmail.com>
2024-12-04 19:03:08 +0100

    fix chat scroll down every 2 seconds

--- web/src/ts/chat-scroll.ts
+++ web/src/ts/chat-scroll.ts
@@ -5,7 +5,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const chat = document.getElementById("chat");
     if (!chat) return;
     findLastMessage()?.scrollIntoView();
-    document.addEventListener("htmx:wsAfterMessage", function () {
+    document.addEventListener("htmx:wsAfterMessage", function (event) {
+        if((event as CustomEvent).detail.message == "") {
+            return
+        }
         // scroll down
         const isScrolledToBottom =
             chat.scrollHeight - chat.scrollTop - chat.clientHeight < 450;
