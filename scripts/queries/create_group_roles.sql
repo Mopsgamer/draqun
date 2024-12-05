db3728079df1c408374a5df9b42f02ba88e8ce94
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-12-05 21:17:21 +0100

    fix init roles db

--- scripts/queries/create_group_roles.sql
+++ scripts/queries/create_group_roles.sql
@@ -9,6 +9,5 @@ CREATE TABLE IF NOT EXISTS app_group_roles (
     perm_ban BIT NOT NULL,
     perm_change_group BIT NOT NULL,
     perm_change_member BIT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (group_id) REFERENCES app_groups (id)
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'Restapp all groups roles';
