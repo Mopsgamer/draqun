499520123802664d695dc158c9aa7e50088ad616
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-12-24 18:39:57 +0100

    improve gql sandbox for mobile devices

---
+++ web/src/ts/docs.ts
@@ -0,0 +1,47 @@
+import type { SlButton, SlTextarea } from "@shoelace-style/shoelace";
+
+async function gqlSend(query: string, outp: HTMLDivElement) {
+    try {
+        const response = await fetch("/gql", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query, // GraphQL expects a `query` property
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        outp.textContent = JSON.stringify(result, null, "  ");
+    } catch (error) {
+        outp.textContent = `Error: ${
+            error instanceof Error ? error.message : error
+        }`;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const inp = document.getElementById("gql-input")! as SlTextarea;
+    const btn = document.getElementById("gql-send")! as SlButton;
+    const outp = document.getElementById("gql-output")! as HTMLDivElement;
+
+    customElements.whenDefined("sl-textarea").then(() =>
+        gqlSend(inp.value, outp)
+    );
+
+    btn.addEventListener("click", () => {
+        gqlSend(inp.value, outp);
+    });
+
+    inp.addEventListener("keydown", (event) => {
+        if (event.key !== "Enter" || !event.ctrlKey) {
+            return;
+        }
+        gqlSend(inp.value, outp);
+    });
+});
