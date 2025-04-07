32bb39229343cc1df9cb32d7fc4a5eb85f9a4d1d
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-04-07 22:49:18 +0200

    change tailwind source; add ws logging

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,11 +1,4 @@
 import type { Config } from "tailwindcss";
 const config: Config = {
-    content: {
-        relative: true,
-        files: [
-            "./templates/**/*",
-            "./src/**/*",
-        ],
-    },
 };
 export default config;
--- server/app.go
+++ server/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/fs"
 	"reflect"
+	"time"
 
 	"github.com/Mopsgamer/draqun/server/controller"
 	"github.com/Mopsgamer/draqun/server/controller/controller_http"
@@ -82,6 +83,27 @@ func NewApp(embedFS fs.FS) (*fiber.App, error) {
 		})
 	}
 
+	logWS := func(start time.Time, err error, ws *controller_ws.ControllerWs) {
+		colorErr := fiber.DefaultColors.Green
+		if err != nil {
+			colorErr = fiber.DefaultColors.Red
+		}
+
+		fmt.Printf(
+			"%s | %s%3s%s | %13s | %15s | %d | %s%s%s \n",
+			time.Now().Format("15:04:05"),
+			colorErr,
+			"ws",
+			fiber.DefaultColors.Reset,
+			time.Since(start),
+			ws.IP,
+			ws.MessageType,
+			fiber.DefaultColors.Yellow,
+			ws.Message,
+			fiber.DefaultColors.Reset,
+		)
+	}
+
 	UseWs := func(
 		subscribe []controller_ws.Subscription,
 		handler func(ctl *controller_ws.ControllerWs) error,
@@ -106,29 +128,13 @@ func NewApp(embedFS fs.FS) (*fiber.App, error) {
 						break
 					}
 
-					// start := time.Now()
+					start := time.Now()
 					ctlWs.MessageType = messageType
 					ctlWs.Message = message
 					err = handler(ctlWs)
 
-					// colorErr := fiber.DefaultColors.Green
-					// if err != nil {
-					// 	colorErr = fiber.DefaultColors.Red
-					// }
+					logWS(start, err, ctlWs)
 
-					// fmt.Printf(
-					// 	"%s | %s%3s%s | %13s | %15s | %d | %s%s%s \n",
-					// 	time.Now().Format("15:04:05"),
-					// 	colorErr,
-					// 	"ws",
-					// 	fiber.DefaultColors.Reset,
-					// 	time.Since(start),
-					// 	ws.IP,
-					// 	ws.MessageType,
-					// 	fiber.DefaultColors.Yellow,
-					// 	ws.Message,
-					// 	fiber.DefaultColors.Reset,
-					// )
 					if err != nil {
 						break
 					}
