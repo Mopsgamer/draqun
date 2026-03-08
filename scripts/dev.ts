7a38b4d342be71567e428b6ee415087fd09ba766
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-03-08 17:43:26 +0100

    chore: dev

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,29 +1,39 @@
 import kill from "tree-kill";
+import { sep } from "node:path";
 import { existsSync } from "@std/fs";
 import { logDevelopment, taskDotenv } from "./tool/constants.ts";
 
 taskDotenv(logDevelopment);
 
-const paths = ["server/", "client-lite.go", "main.go", ".git/ORIG_HEAD"];
+const paths = [
+    "server" + sep,
+    "client-lite.go",
+    "main.go",
+    ".git" + sep + "ORIG_HEAD",
+];
 if (existsSync(".env")) paths.push(".env");
-const generateCommand = new Deno.Command("go", {
-    args: ["generate", "./..."],
-});
-const serverCommand = new Deno.Command("go", {
-    args: ["run", "-tags", "lite", "."],
-});
-let goRunProcess: Deno.ChildProcess | undefined = undefined;
 
-async function start(): Promise<void> {
-    goRunProcess = generateCommand.spawn();
-    const { success } = await goRunProcess.output();
-    if (!success) return;
-    goRunProcess = serverCommand.spawn();
+async function start(signal?: AbortSignal): Promise<number> {
+    const generateCommand = new Deno.Command("go", {
+        args: ["generate", "./..."],
+        signal,
+    });
+    const { success } = await generateCommand.output();
+    if (!success) return NaN;
+
+    const serverCommand = new Deno.Command("go", {
+        args: ["run", "-tags", "lite", "."],
+        signal,
+    });
+    return serverCommand.spawn().pid;
 }
 
 async function watchAndRestart(): Promise<void> {
     const watcher = Deno.watchFs(paths, { recursive: true });
     await start();
+    let pid = NaN;
+    let controller: AbortController | undefined;
+    let timeout = NaN;
     for await (const event of watcher) {
         if (
             !(
@@ -32,19 +42,26 @@ async function watchAndRestart(): Promise<void> {
             )
         ) continue;
 
-        tryToKill();
-        await start();
+        clearTimeout(timeout);
+
+        timeout = setTimeout(async () => {
+            if (!isNaN(pid)) {
+                controller!.abort();
+                tryToKill(pid);
+            }
+            controller = new AbortController();
+            await logDevelopment.info(
+                "Refreshing (" + event.kind + " " + event.paths + ")",
+            );
+            pid = await start(controller.signal);
+        }, 50);
     }
 }
 
-function tryToKill(): void {
-    if (goRunProcess == undefined) {
-        return;
-    }
+function tryToKill(pid: number): void {
     try {
-        kill(goRunProcess.pid, "SIGTERM");
+        kill(pid, "SIGTERM");
     } catch { /* empty */ }
-    goRunProcess = undefined;
 }
 
 watchAndRestart();
